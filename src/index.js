4aaa4f3c562d7bf08954fdca0dee0af92c5848f5
Lucas Cassimiro <lucas.cassimir@gmail.com>
2020-06-10 01:13:59 -0300

    Remoção de arquivos em desuso, organização das pages em pastas e criação de folhas de estilo css

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import Repositories from './Repositories';
-import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App title="Bem vindo a minha página!!" />
+    <App title = "Bem vindo a minha página!!" />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
 
-serviceWorker.unregister();
